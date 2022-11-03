116349c2812da9aef83e2d2482b7266bd79de3e6
FrancoVM\Franco <francocabello440@gmail.com>
2022-11-03 16:44:13 -0400

    Add basic tests

--- Fit-Test/UnitTest1.cs
+++ Fit-Test/UnitTest1.cs
@@ -6,6 +6,14 @@ namespace Fit_Test
         [TestMethod]
         public void TestMethod1()
         {
+            var (width3, height3) = Fit.Calculate.GetSize(10, 5, 10, 5);
+            Assert.AreEqual(10, width3);
         }
-    }
+		[TestMethod]
+		public void TestMethod2()
+		{
+			var (width3, height3) = Fit.Calculate.GetSize(10, 5, 10, 5);
+			Assert.AreEqual(5, height3);
+		}
+	}
 }
---
+++ Fit/Calculate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fit
+{
+	public class Calculate
+	{
+		public static (int width1, int height1) GetSize(int width1, int height1, int width2, int height2)
+		{
+			return (width1, height1);
+		}
+	}
+}
