87fca2a6d0613d9f98158b78fa09b6ebf31fdb4b
JossephVM\Josseph <jossephabrahamm@gmail.com>
2022-11-17 19:53:30 +0000

    Console created.

--- Fit/Program.cs
+++ Fit/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Fit;
+
+Console.WriteLine("Enter width1");
+string str1 = Console.ReadLine();
+Console.WriteLine("Enter height1");
+string str2 = Console.ReadLine();
+Console.WriteLine("Enter width2");
+string str3 = Console.ReadLine();
+Console.WriteLine("Enter height2");
+string str4 = Console.ReadLine();
+
+var rw1 = double.Parse(str1);
+var rw2 = double.Parse(str3);
+var rh1 = double.Parse(str2);
+var rh2 = double.Parse(str4);
+
+var result = Calculate.GetSize(rw1, rh1, rw2, rh2);
+Console.WriteLine("New width is " + result.width3 + " new height is " + result.height3);
